2710126bcad400183a78380d2368d37f61f1ece6
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-06-23 14:03:58 -0600

    Add the NOAA favicon

--- services/frontend/src/server.js
+++ services/frontend/src/server.js
@@ -1,8 +1,14 @@
 const debug = require("debug")("HRRRSmoke");
 const express = require("express");
 
+const path = require("path");
+
 const app = express();
 const port = process.env.HTTP_PORT || 3000;
+
+app.get("/favicon.ico", function (req, res) {
+  res.sendFile(path.join(__dirname, "favicon.ico"));
+});
 
 app.get("/", function (req, res) {
   res.send("Hello");
