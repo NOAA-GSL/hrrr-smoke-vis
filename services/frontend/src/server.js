3a3aacb516122c9c35eb6de284c0dd05888ace80
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-06-23 14:51:51 -0600

    Add a few comments

--- services/frontend/src/server.js
+++ services/frontend/src/server.js
@@ -7,6 +7,7 @@ const path = require("path");
 const app = express();
 const port = process.env.HTTP_PORT || 3000;
 
+// Set up Nunjucks for express. Only watch files in development.
 nunjucks.configure(path.join(__dirname, "views"), {
   autoescape: true,
   express: app,
@@ -15,20 +16,24 @@ nunjucks.configure(path.join(__dirname, "views"), {
 
 app.set("view engine", "njk");
 
+// Serve static files in development.
 if (process.env.NODE_ENV === "development") {
   app.use("/css", express.static(path.join(__dirname, "static", "css")));
   app.use("/img", express.static(path.join(__dirname, "static", "img")));
   app.use("/js", express.static(path.join(__dirname, "static", "js")));
 }
 
+// Nothing fancy, just a favicon.
 app.get("/favicon.ico", function (req, res) {
   res.sendFile(path.join(__dirname, "favicon.ico"));
 });
 
+// Application landing page
 app.get("/", function (req, res) {
   res.render("index");
 });
 
+// Run the app. The URL is logged using debug to the HRRRSmoke namespace.
 app.listen(port, function () {
   debug(`Listening at http://localhost:${port}`);
 });
