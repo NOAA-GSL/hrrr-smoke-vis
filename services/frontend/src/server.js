a1e506ad18ec9cabe9728669aec21685e928f825
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-06-23 14:22:32 -0600

    Configure Nunjucks
    Set up a basic structure for the Nunjucks templates along and configure
    Nunjucks to work with Express.

--- services/frontend/src/server.js
+++ services/frontend/src/server.js
@@ -1,17 +1,26 @@
 const debug = require("debug")("HRRRSmoke");
 const express = require("express");
+const nunjucks = require("nunjucks");
 
 const path = require("path");
 
 const app = express();
 const port = process.env.HTTP_PORT || 3000;
 
+nunjucks.configure(path.join(__dirname, "views"), {
+  autoescape: true,
+  express: app,
+  watch: process.env.NODE_ENV === "development",
+});
+
+app.set("view engine", "njk");
+
 app.get("/favicon.ico", function (req, res) {
   res.sendFile(path.join(__dirname, "favicon.ico"));
 });
 
 app.get("/", function (req, res) {
-  res.send("Hello");
+  res.render("index");
 });
 
 app.listen(port, function () {
