eef8fda829437a274e6b0d3f87d2f82bfd086669
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-08-13 13:51:00 -0600

    Get the SPA dev environment up and running
    Set up scripts to run the server with sirv-cli and handle copying of
    static files over

--- services/frontend/rollup.config.js
+++ services/frontend/rollup.config.js
@@ -1,14 +1,61 @@
+import css from "rollup-plugin-css-only";
+import livereload from "rollup-plugin-livereload";
 import resolve from "@rollup/plugin-node-resolve";
+import svelte from "rollup-plugin-svelte";
+import { terser } from "rollup-plugin-terser";
+
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+  let server;
+
+  function toExit() {
+    if (server) server.kill(0);
+  }
+
+  return {
+    writeBundle() {
+      if (server) return;
+
+      server = require("child_process").spawn(
+        "npm",
+        ["run", "serve"],
+        {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true,
+        }
+      );
+
+      process.on("SIGTERM", toExit);
+      process.on("exit", toExit);
+    }
+  };
+}
 
 export default {
-  input: {
-    HrrrMap: "src/components/HrrrMap.js",
-  },
+  input: "src/main.js",
   output: {
-    dir: "public/js/",
-    format: "es"
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/js/main.js"
   },
   plugins: [
-    resolve(),
-  ]
+    svelte({
+      compilerOptions: {
+        dev: !production,
+      },
+    }),
+    css({ output: "app.css" }),
+    resolve({
+      browser: true,
+      dedupe: ["svelte"]
+    }),
+    !production && serve(),
+    !production && livereload("public"),
+    production && terser(),
+  ],
+  watch: {
+    clearScreen: false,
+  },
 };
