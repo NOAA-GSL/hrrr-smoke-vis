6963a24aad9648bee0e4bb9389211d38ac09c4e7
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-08-14 11:50:48 -0600

    Update tests
    Update the tests from the previous placeholder checks to confirm that we
    have some of the UI we need. I'm not really testing the map component
    itself right now because I think I need some visual regression testing
    for that.

--- services/frontend/cypress/integration/home_spec.js
+++ services/frontend/cypress/integration/home_spec.js
@@ -1,18 +1,23 @@
 describe("Home", function () {
   before(function () {
-    cy.visit("http://localhost:3000/");
+    cy.visit("http://localhost:8080/");
   });
 
   it("has a title", function () {
     cy.title().should("eq", "HRRR Smoke Vis [EXPERIMENTAL]");
-    cy.get("h1").contains("HRRR Smoke Vis");
+    cy.get("h1").contains("HRRR Smoke");
   });
 
   it("is labelled experimental", function () {
     cy.contains("Experimental");
   });
 
-  it("displays a placeholder message", function () {
-    cy.contains("Coming soon!");
+  it("has a checkbox for showing counties", function () {
+    cy.get("#showCounties")
+      .should("have.attr", "type", "checkbox")
+      .and("not.have.attr", "checked");
+
+    cy.get("label[for='showCounties']")
+      .contains("Show Counties");
   });
 });
