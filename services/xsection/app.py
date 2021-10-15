94b63914d34df5d81b4bd79ad8526ced957327a1
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-10-15 13:57:06 -0600

    Read a test JSON file from S3
    I've updated the Lambda handler to fetch a JSON file from an AWS bucket.
    This was a test to understand how to fetch objects from S3 and provide
    configuration to the handler via environment variables.

--- services/xsection/app.py
+++ services/xsection/app.py
@@ -1,4 +1,17 @@
+from urllib.request import urlopen
+import os
+
+
 def handler(event, context):
-    return {
-        "message": "hello, world"
-    }
+    region = os.environ.get("AWS_REGION", "us-east-1")
+
+    bucket = os.environ["HRRR_SMOKE_BUCKET"]
+    file_path = "smoke/hello.json"
+
+    bucket_url = f"https://s3.{region}.amazonaws.com/{bucket}/{file_path}"
+    print(bucket_url)
+
+    with urlopen(bucket_url) as response:
+        data = response.read()
+
+    return data
