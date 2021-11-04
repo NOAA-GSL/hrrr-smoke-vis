a77dbac9986a10acab7638a2ab223a219a94f8bf
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-11-04 08:58:29 -0600

    Set the URL for the backend as an env variable
    I added the rollup-replace plugin so that I could replace text in the
    code using environment variables. This will allow me to configure the
    backend for our docker environment locally in development, and for AWS
    when building for production.

--- services/frontend/rollup.config.js
+++ services/frontend/rollup.config.js
@@ -1,5 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
@@ -46,6 +47,10 @@ export default {
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
+
+		replace({
+			__HRRR_XSECTION_API__: JSON.stringify(process.env.HRRR_XSECTION_API),
+		}),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
