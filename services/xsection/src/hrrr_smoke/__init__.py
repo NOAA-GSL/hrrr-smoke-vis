06f74e58813a5b6d4a33671b77030c28fa4f8ee2
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-11-17 08:28:42 -0700

    Convert the Lambda function to a Flask route
    Converted the handler for the Lambda function to a regular Flask route
    in a blueprint. I had to add s3fs as a dependency for now just to avoid
    having to deal with accessing GRIB files locally. I think that will be
    in a subsequent branch. For now the goal is to just get what was a
    Lambda function converted to a Flask app.

--- services/xsection/src/hrrr_smoke/__init__.py
+++ services/xsection/src/hrrr_smoke/__init__.py
@@ -3,11 +3,11 @@ import os
 from flask import Flask
 
 
-def index():
-    return "<h1>Hello, World</h1>"
-
-
 def create_app(config=None):
     app = Flask(__name__)
-    app.add_url_rule("/", view_func=index)
+
+    from . import routes
+
+    app.register_blueprint(routes.bp)
+
     return app
