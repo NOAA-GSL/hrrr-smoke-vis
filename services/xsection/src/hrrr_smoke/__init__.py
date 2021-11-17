c7407ee6d3080253e4cd7b4a45e06dacbd9ab8f9
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-11-17 06:52:18 -0700

    Set up poetry for packaging
    Replace requirements.txt with a pyproject.toml file for poetry to handle
    building and installing the project. Update the Docker configuration for
    these changes as well.

--- services/xsection/src/hrrr_smoke/__init__.py
+++ services/xsection/src/hrrr_smoke/__init__.py
@@ -3,7 +3,11 @@ import os
 import flask
 
 
+def index():
+    return "<h1>Hello, World</h1>"
+
+
 def create_app(config=None):
     app = Flask(__name__)
-
+    app.add_url_rule("/", index)
     return app
