9fc758cc0cfaa5404fd9515462235c740b1680fa
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-12-01 08:37:41 -0700

    Implement forecast selection for HRRR controls
    Added two dropdowns to the HRRR controls for forecast selection. I'm
    thinking it might be best to move these off to a separate component to
    keep the fetching of the forecasts isolated from the rest of the
    controls and only expose the selected forecast so that the controls can
    set the appropriate variable in the store when "Update" is clicked.

--- services/frontend/src/components/HrrrControls.svelte
+++ services/frontend/src/components/HrrrControls.svelte
@@ -1,9 +1,29 @@
 <script>
-  import { path } from "../stores.js";
+  import { onMount } from "svelte";
+
+  import { path, HRRR_XSECTION_API } from "../stores.js";
   import CoordinateInput from "./CoordinateInput.svelte";
+  import { Dropdown } from "./uswds";
 
   let start = { "lat": null, "lng": null };
   let end = { "lat": null, "lng": null };
+
+  let forecasts = {};
+  $: runTimes = Object.keys(forecasts).map(function (k) {
+    return { value: k, text: k };
+  });
+
+  let run = "";
+  $: forecastHours = (forecasts[run] || []).map(function (f) {
+    return { value: f.forecast, text: `${f.forecastHour} (${f.validTime})` };
+  });
+
+  let forecast;
+
+  onMount(async function () {
+    forecasts = await fetch(`${HRRR_XSECTION_API}/forecasts/`)
+      .then((response) => response.json());
+  });
 
   function update() {
     path.set({
@@ -16,6 +36,10 @@
 </script>
 
 <section class="hrrr-controls stack" aria-label="Controls">
+  <h2>Forecast</h2>
+  <Dropdown id="run-time" label="Run Time" options={runTimes} bind:selected={run} />
+  <Dropdown id="forecast-hour" label="Forecast Hour" options={forecastHours} bind:selected={forecast} />
+
   <h2>Cross-section Path</h2>
   <CoordinateInput id="start" label="Start" coordinate={start} />
   <CoordinateInput id="end" label="End" coordinate={end} />
