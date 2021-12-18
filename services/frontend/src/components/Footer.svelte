680d949ad42ee1b4bf868e5765bbb73eacdea3a6
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-12-18 11:13:45 -0700

    Center the footer links

--- services/frontend/src/components/Footer.svelte
+++ services/frontend/src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer>
-  <p class="cluster">
+  <p class="cluster -center">
     <a href="https://github.com/NOAA-GSL/hrrr-smoke-vis/issues/new?assignees=&labels=enhancement&template=bug_report.md&title=">Report a Bug</a>
     &#8226;
     <a href="https://github.com/NOAA-GSL/hrrr-smoke-vis/issues/new?assignees=&labels=enhancement&template=feature_request.md&title=">Request a Feature</a>
