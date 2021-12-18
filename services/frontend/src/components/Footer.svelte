0ab3125df743b72f87aff994eb6cb5646d53499e
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-12-18 11:10:06 -0700

    Add a footer with links to the GitHub issue templates

---
+++ services/frontend/src/components/Footer.svelte
@@ -0,0 +1,7 @@
+<footer>
+  <p class="cluster">
+    <a href="https://github.com/NOAA-GSL/hrrr-smoke-vis/issues/new?assignees=&labels=enhancement&template=bug_report.md&title=">Report a Bug</a>
+    &#8226;
+    <a href="https://github.com/NOAA-GSL/hrrr-smoke-vis/issues/new?assignees=&labels=enhancement&template=feature_request.md&title=">Request a Feature</a>
+  </p>
+</footer>
