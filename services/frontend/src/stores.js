54df5d484debfc624ff168ff8236c1223642d87f
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-02-11 08:24:05 -0700

    Add JSDoc to the stores
    Since most of these stores default to null values, it's not obvious what
    should go in them, so I added some JSDoc to define the types and
    properties on them.

--- services/frontend/src/stores.js
+++ services/frontend/src/stores.js
@@ -1,9 +1,28 @@
+/** @module stores */
 import { derived, writable } from "svelte/store";
 import * as api from "./api.js";
 
+/**
+ * Forecast initialization time in ISO 8601 format.
+ * @type {?string}
+ */
 export const runHour = writable(null);
+
+/**
+ * Offset in hours from the initialization time of the forecast.
+ * @type {?number}
+ */
 export const validTime = writable(null);
 
+/**
+ * Definition of the path across which the cross-section is calculated.
+ *
+ * @type {?Object}
+ * @property {number} startLat - Starting latitude of the path
+ * @property {number} startLng - Starting longitude of the path
+ * @property {number} endLat   - Ending latitude of the path
+ * @property {number} endLng   - Ending longitude of the path
+ */
 export const path = writable(null);
 
 const emptyXSection = {
@@ -12,6 +31,14 @@ const emptyXSection = {
   columns: 0,
 };
 
+/**
+ * Cross-section of smoke density
+ *
+ * @type {Object}
+ * @property {Array.number} massden - 1D array of smoke density in micrograms / meter
+ * @property {number}       rows    - Number of rows in the dataset
+ * @property {number}       columns - Number of columns in the dataset
+ */
 export const xsection = derived(
   [runHour, validTime, path],
   ([$runHour, $validTime, $path], set) => {
