575e5db8fb725bbf45c63ee16dfa987e16fbe4bf
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-02-28 08:37:52 -0700

    Remove padding from the legend
    The legend was getting some padding from the global styles which was
    messing up the layout. I've removed it explicitly in the legend
    component, but it makes me wonder if maybe a little refactor is in
    order.

--- services/frontend/src/components/Legend.svelte
+++ services/frontend/src/components/Legend.svelte
@@ -1,27 +1,30 @@
 <script>
   import { extent } from "d3-array";
-  import { scaleLinear } from "d3-scale";
   import { smokeScale, thresholds } from "../stores.js";
 
   let width = 0;
   let height = 0;
 
-  $: y = scaleLinear().domain(extent($thresholds)).range([height, 0]);
+  $: swatchHeight = height / ($thresholds.length - 1);
   $: ticks = $thresholds.map((t, idx, arr) => [t, arr[idx + 1]]);
 </script>
 <div class="legend" bind:offsetWidth={width} bind:offsetHeight={height}>
   <svg viewBox="0 0 {width} {height}" {width} {height}>
-    {#each ticks as tick}
+    {#each ticks as tick, idx}
       {#if isFinite(tick[1])}
         <rect class="swatch"
-              y={y(tick[1])}
-              {width} height={y(tick[0]) - y(tick[1])}
+              y={height - (idx + 1) * swatchHeight}
+              {width} height={swatchHeight}
               fill={$smokeScale(tick[0])} />
       {/if}
     {/each}
   </svg>
 </div>
 <style>
+  .legend {
+    padding: 0;
+  }
+
   .swatch {
     stroke: white;
     stroke-width: 1px;
