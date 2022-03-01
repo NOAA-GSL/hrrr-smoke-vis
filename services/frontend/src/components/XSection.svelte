47eb9746534600e7c644a3bfaeff8c571cc586cf
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-03-01 09:02:40 -0700

    Add a loading spinner to the XSection component
    I've partially converted the XSection component to work directly with
    promises so that we can make use of our loading spinner. It's not quite
    working, though, because my fan spins up and the data never displays,
    which makes me think I'm mishandling some promise states here.
    
    I'm finding it a little tricky to reason about promises with Svelte's
    reactivity.

--- services/frontend/src/components/XSection.svelte
+++ services/frontend/src/components/XSection.svelte
@@ -1,9 +1,17 @@
 <script>
-  import { smokeScale, thresholds, xsection } from "../stores.js";
+  import {
+    path,
+    runHour,
+    smokeScale,
+    thresholds,
+    validTime
+  } from "../stores.js";
+  import * as api from "../api.js";
   import AxisBottom from "./AxisBottom.svelte";
   import AxisLeft from "./AxisLeft.svelte";
   import Contour from "./Contour.svelte";
   import HrrrMap from "./HrrrMap.svelte";
+  import Loading from "./Loading.svelte";
 
   import {
     contours,
@@ -16,34 +24,52 @@
   let width = 0;
   let height = 0;
   let mapSize = 0;
+  let smoke = [];
+  let xScale = scaleLinear();
+  let yScale = scaleLinear();
+  let contourPath;
 
-  $: smoke = contours().size([$xsection.columns, $xsection.rows]).thresholds($thresholds)($xsection.massden);
-  $: xScale = scaleLinear().domain([0, $xsection.columns]).range([0, width]);
-  $: yScale = scaleLinear().domain([0, $xsection.rows]).range([height, 0]);
-  $: path = geoPath(geoTransform({
-    point: function (x, y) {
-      this.stream.point(xScale(x), yScale(y));
-    },
-  }));
+  $: ready = $runHour !== null && $validTime !== null && $path !== null;
+  $: data = ready ? api.xsection({
+    runHour: $runHour,
+    validTime: $validTime,
+    ...$path
+  }) : Promise.resolve(null)
+
+  $: data.then(function (xsection) {
+    if (xsection === null) return;
+    smoke = contours().size([xsection.columns, xsection.rows]).thresholds($thresholds)(xsection.massden);
+    xScale = scaleLinear().domain([0, xsection.columns]).range([0, width]);
+    yScale = scaleLinear().domain([0, xsection.rows]).range([height, 0]);
+    contourPath = geoPath(geoTransform({
+      point: function (x, y) {
+        this.stream.point(xScale(x), yScale(y));
+      },
+    }));
+  });
 </script>
 
-<div class="hrrr-xsection container">
-  <div class="chart" bind:offsetWidth={width} bind:offsetHeight={height}>
-    <svg class="x-section" viewBox="0 0 {width} {height}">
-      <Contour contours={smoke} fill={$smokeScale} {path} />
-      <g class="axis">
-        <AxisLeft scale={yScale} />
-      </g>
-      <AxisBottom scale={xScale} transform="translate(0, {height})" />
-    </svg>
-  </div>
+<div class="hrrr-xsection">
+  <Loading promise={data}>
+    <div class="container">
+      <div class="chart" bind:offsetWidth={width} bind:offsetHeight={height}>
+        <svg class="x-section" viewBox="0 0 {width} {height}">
+          <Contour contours={smoke} fill={$smokeScale} path={contourPath} />
+          <g class="axis">
+            <AxisLeft scale={yScale} />
+          </g>
+          <AxisBottom scale={xScale} transform="translate(0, {height})" />
+        </svg>
+      </div>
 
-  <div class="map" bind:offsetWidth={mapSize}>
-    <HrrrMap width={mapSize} height={mapSize} />
-  </div>
+      <div class="map" bind:offsetWidth={mapSize}>
+        <HrrrMap width={mapSize} height={mapSize} />
+      </div>
 
-  <small class="axis-title left">Pressure (mb, from Standard Atmosphere)</small>
-  <small class="axis-title bottom">Distance (km)</small>
+      <small class="axis-title left">Pressure (mb, from Standard Atmosphere)</small>
+      <small class="axis-title bottom">Distance (km)</small>
+    </div>
+  </Loading>
 </div>
 
 <style>
