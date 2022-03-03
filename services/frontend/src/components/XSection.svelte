9f74b324411a181486a86fe75ef3dd1f2840ba8b
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-03-03 15:27:40 -0700

    Remove unused map sizing props from XSection
    The map sizes itself, so I don't need to pass in these sizing props
    any more; the container keeps the map to the right size.

--- services/frontend/src/components/XSection.svelte
+++ services/frontend/src/components/XSection.svelte
@@ -25,7 +25,6 @@
 
   let width = 0;
   let height = 0;
-  let mapSize = 0;
 
   let columns = 0;
   let rows = 0;
@@ -71,8 +70,8 @@
       </svg>
     </div>
 
-    <div class="map" bind:offsetWidth={mapSize}>
-      <HrrrMap width={mapSize} height={mapSize} data={mapData} />
+    <div class="map">
+      <HrrrMap data={mapData} />
     </div>
 
     <small class="axis-title left">Pressure (mb, from Standard Atmosphere)</small>
