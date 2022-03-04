33d022e95dc2b7ed5d8719670ae97b49919e3120
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-03-04 16:00:03 -0700

    Move styles for coordinate inputs into the component
    First test of the SCSS preprocessing in component styles is successful.
    The styles for the coordinate input are now in the Svelte component and
    successfully reading the gap size from the USWDS functions. I also
    copied the label styles for the uppercase labels into the FormGroup
    component, though I think I'll want to create some font tokens as
    utility classes and use those instead.

--- services/frontend/src/components/uswds/FormGroup.svelte
+++ services/frontend/src/components/uswds/FormGroup.svelte
@@ -1,9 +1,20 @@
 <script>
   export let id;
   export let label;
+  export let labelClass = ""
 </script>
 
 <div class="flow">
-  <label for={id}>{label}</label>
+  <label for={id} class="{labelClass}">{label}</label>
   <slot></slot>
 </div>
+
+<style lang="scss">
+  @use "uswds";
+  .uppercase {
+    font-size: uswds.size("ui", "3xs");
+    font-weight: bold;
+    letter-spacing: 0.03em;
+    text-transform: uppercase;
+  }
+</style>
